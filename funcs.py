501e13f427015f77dbf9cde26e2f85c71da41d68
lolacostul <lolapnnt@yahoo.com>
2021-12-16 16:38:16 +0100

    ajout fonction moyenne mean_function

--- funcs.py
+++ funcs.py
@@ -2,3 +2,9 @@ def min_function(a, b):
 	if a < b:
 		return a
 	return b
+
+def mean_function(int_list):
+	list_sum = 0
+	for nb in int_list:
+		list_sum += nb
+	return list_sum // len(int_list)
--- tests_funcs.py
+++ tests_funcs.py
@@ -10,5 +10,13 @@ class TestFuncs(unittest.TestCase):
         self.assertEqual(funcs.min_function(-1, -10), -10)
         self.assertEqual(funcs.min_function(0, 0), 0)
 
+    def test_mean_int(self):
+        self.assertEqual(funcs.mean_function([0,1,2,3,4]), 2)
+        self.assertEqual(funcs.mean_function([0,0,0,0,0]), 0)
+        self.assertEqual(funcs.mean_function([2,2]), 2)
+        self.assertEqual(funcs.mean_function([-10,10]), 0)
+        self.assertEqual(funcs.mean_function([-4,10,25,-12]), 4)
+        self.assertEqual(funcs.mean_function([-5,7,3,-3,2]), 0)
+
 if __name__ == '__main__':
     unittest.main()
