a56d05a13f8e6429d90c6d41970f86d078f2a6ef
Maxime Lebreton <maxime.lebreton@etu.upmc.fr>
2021-12-16 16:20:52 +0100

    correction bug test min

--- tests_funcs.py
+++ tests_funcs.py
@@ -1,14 +1,14 @@
-import min_function
+import funcs
 import unittest
 
 class TestFuncs(unittest.TestCase):
 
     def test_min_int(self):
-        self.assertEqual(min_function.min_function(0, 2), 0)
-        self.assertEqual(min_function.min_function(8, -5), -5)
-        self.assertEqual(min_function.min_function(-1, 2), -1)
-        self.assertEqual(min_function.min_function(-1, -10), -10)
-        self.assertEqual(min_function.min_function(0, 0), 0)
+        self.assertEqual(funcs.min_function(0, 2), 0)
+        self.assertEqual(funcs.min_function(8, -5), -5)
+        self.assertEqual(funcs.min_function(-1, 2), -1)
+        self.assertEqual(funcs.min_function(-1, -10), -10)
+        self.assertEqual(funcs.min_function(0, 0), 0)
 
 if __name__ == '__main__':
     unittest.main()
