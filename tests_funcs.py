955580776fe18d12e67f7f986718267c06051733
Maxime Lebreton <maxime.lebreton@etu.upmc.fr>
2021-12-16 17:16:07 +0100

    test ecart type

--- tests_funcs.py
+++ tests_funcs.py
@@ -16,5 +16,11 @@ class TestFuncs(unittest.TestCase):
         self.assertEqual(funcs.mediane_function([-10,-7,3,6,12]),3)
         self.assertEqual(funcs.mediane_function([0,0,0,0,0,0,0,0,1]),0)
 
+    def test_ecart_type_f32(self):
+        self.assertEqual(funcs.ecart_type_function([1,2,3]),0.8164966)
+        self.assertEqual(funcs.ecart_type_function([1,2,3,5,5,6]),1.7078252)
+        self.assertEqual(funcs.ecart_type_function([-10,-7,3,6,12]),8.182909)
+        self.assertEqual(funcs.ecart_type_function([0,0,0,0,0,0,0,0,1]),0.3142697)
+
 if __name__ == '__main__':
     unittest.main()
