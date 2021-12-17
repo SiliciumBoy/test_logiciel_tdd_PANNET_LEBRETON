178de601f24c949003f136749a41f672565b042d
lolacostul <lolapnnt@yahoo.com>
2021-12-17 09:00:21 +0100

    Resolution pb

--- funcs.py
+++ funcs.py
@@ -5,13 +5,11 @@ def min_function(a, b):
 		return a
 	return b
 
-<<<<<<< HEAD
 def mean_function(int_list):
 	list_sum = 0
 	for nb in int_list:
 		list_sum += nb
 	return list_sum // len(int_list)
-=======
+
 def mediane_function(a):
 	return np.median(a)
->>>>>>> 955580776fe18d12e67f7f986718267c06051733
--- tests_funcs.py
+++ tests_funcs.py
@@ -10,7 +10,6 @@ class TestFuncs(unittest.TestCase):
         self.assertEqual(funcs.min_function(-1, -10), -10)
         self.assertEqual(funcs.min_function(0, 0), 0)
 
-<<<<<<< HEAD
     def test_mean_int(self):
         self.assertEqual(funcs.mean_function([0,1,2,3,4]), 2)
         self.assertEqual(funcs.mean_function([0,0,0,0,0]), 0)
@@ -18,7 +17,7 @@ class TestFuncs(unittest.TestCase):
         self.assertEqual(funcs.mean_function([-10,10]), 0)
         self.assertEqual(funcs.mean_function([-4,10,25,-12]), 4)
         self.assertEqual(funcs.mean_function([-5,7,3,-3,2]), 0)
-=======
+
     def test_mediane_int(self):
         self.assertEqual(funcs.mediane_function([1,2,3]),2)
         self.assertEqual(funcs.mediane_function([1,2,3,5,5,6]),4)
@@ -30,7 +29,6 @@ class TestFuncs(unittest.TestCase):
         self.assertEqual(funcs.ecart_type_function([1,2,3,5,5,6]),1.7078252)
         self.assertEqual(funcs.ecart_type_function([-10,-7,3,6,12]),8.182909)
         self.assertEqual(funcs.ecart_type_function([0,0,0,0,0,0,0,0,1]),0.3142697)
->>>>>>> 955580776fe18d12e67f7f986718267c06051733
 
 if __name__ == '__main__':
     unittest.main()
